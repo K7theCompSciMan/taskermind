7b33481a8e8e9e44e477fb17c2387497fc9cff54
Kesavan Rangarajan <k7rang2018@gmail.com>
2024-02-05 11:23:36 -0500

    added nav

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,19 @@
-<script>
+<script lang="ts">
     import "../app.css"
+    import { goto } from "$app/navigation";
+    import Button from "$lib/Button.svelte";
+    type Page = {
+        name: string;
+        path: string;}
+
+    let pages: Page[] = [
+        {name: "Home", path: "/"}
+    ]
 </script>
 
+<nav class="justify-center items-center text-center">
+    {#each pages as page}
+        <Button name={page.name} onclick={() => goto(page.path)} />
+    {/each}
+</nav>
 <slot />
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,3 @@
+<script lang="ts">
+</script>
 <h1 class ="font-bold text-lg justify-center text-center items-center underline bg-blue-500">Hello World</h1>
