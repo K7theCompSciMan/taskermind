8b7d620a6ad2f7f2fdc51f1aa6ee60f88ecc234d
Kesavan Rangarajan <k7rang2018@gmail.com>
2024-02-06 15:40:50 -0500

    s

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,14 +1,3 @@
 <script lang="ts">
 </script>
 <h1 class ="font-bold text-lg justify-center text-center items-center underline">Hello World</h1>
-<h1> Hi w</h1>
-
-<ul> 
-
-    <li> w sucks 1</li>
-    <li> w sucks 2</li>
-    <li> l sucks 3</li>
-    <li> w sucks 4</li>
-    <li> w sucks 5</li>
-    <li> w sucks 6</li>
-</ul>
