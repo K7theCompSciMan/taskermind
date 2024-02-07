8c0c1a354c01544eb34947f2ee8dd85c3e6d40c6
ZanyeNair <zayne.nair@circuitrunners.com>
2024-02-07 00:31:10 -0500

    learn zayneeeeee

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,8 @@
 <script lang="ts">
+
+
 </script>
 <h1 class ="font-bold text-lg justify-center text-center items-center underline">Hello World</h1>
+
+
+
--- src/routes/tasks/+page.svelte
+++ src/routes/tasks/+page.svelte
@@ -1,1 +1,37 @@
-<p1> hello</p1>
+<script>
+    let count = 0;
+
+    function increment() {
+        count += 1;
+    }
+    $: if(count >= 10) {
+        alert("You clicked more than 10 times")
+    }
+
+    function resetPls() {
+        count = 0;
+        alert("Red]set")
+    }
+</script>
+
+
+<button on:click={increment}>
+	Clicked {count}
+	{count === 1 ? 'time' : 'times'}
+    
+</button>
+<button on:click={resetPls}>
+    Reset
+    
+</button>
+
+<style>
+    button {
+        background-color: rgb(49, 15, 145);
+        color: #fff;
+        padding: 2em;
+        border: none;
+        border-radius: 5em;
+        cursor: pointer;
+    }
+</style>
