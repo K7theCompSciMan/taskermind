cffbb065ee2beda6444f09f6d5a623072196b061
Kesavan Rangarajan <k7rang2018@gmail.com>
2024-02-07 10:50:54 -0500

    Updated Login Page and CSS

--- src/lib/Heading.svelte
+++ src/lib/Heading.svelte
@@ -1,10 +1,20 @@
 <script lang="ts">
     export let name = "TaskerMind";
-    export let size = "2xl";
+    export let textsize = "2xl";
     export let color = "black";
     export let weight = "bold";
+    export let size: string = "w-56";
     export let style = " ";
+    export let logo: boolean = false;
+    export let src: string = "";
+    export let onclick: () => void = () => {};
 </script>
 
-
-<h1 class="text-{size} p-2 m-2 bg-orange-500 w-64 border-8 rounded-lg border-gray-300 font-{color} font-{weight} {style}">{name}</h1>
+{#if logo}
+    <img {src} alt="TaskerMind Logo" class="w-20 h-20 m-2 rounded-3xl"/>
+{/if}
+<!-- svelte-ignore a11y-click-events-have-key-events -->
+<!-- svelte-ignore a11y-no-static-element-interactions -->
+<div class="text-{textsize} p-2 m-2 bg-orange-500 {size} border-8 rounded-2xl border-gray-300 font-{color} font-{weight} {style} hover:cursor-pointer" on:click={() =>onclick()}>
+    {name}
+</div>
