ddc839975e628ce794906dad67a4c5954821eb09
Kesavan Rangarajan <k7rang2018@gmail.com>
2024-02-09 11:07:37 -0500

    began to implement into thingie

--- src/lib/Button.svelte
+++ src/lib/Button.svelte
@@ -4,8 +4,9 @@
     export let hover: () => void;
     export let leave: () => void;
     export let size: string = "w-44";
+    export let type: "button" | "submit" | "reset" | null | undefined = "button";
 </script>
 
-<button class="p-2 m-2 rounded-2xl shadow-lg bg-sky-400 {size} hover:bg-sky-500 hover:border-gray-400 hover:shadow-slate-300 transition-shadow" style="transition:0.5s" on:mouseenter={() => hover()} on:click={() => onclick()} on:mouseleave={() => leave()}  
+<button {type} class="p-2 m-2 rounded-2xl shadow-lg bg-sky-400 {size} hover:bg-sky-500 hover:border-gray-400 hover:shadow-slate-300 transition-shadow" style="transition:0.5s" on:mouseenter={() => hover()} on:click={() => onclick()} on:mouseleave={() => leave()}  
 	>{name}</button
 >
