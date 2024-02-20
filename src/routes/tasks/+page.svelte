5e63406ccd332abb15207f84ce80b6d689a7d90a
ZanyeNair <zayne.nair@circuitrunners.com>
2024-02-20 18:21:01 -0500

    try again

--- src/routes/tasks/+page.svelte
+++ src/routes/tasks/+page.svelte
@@ -1,29 +1,40 @@
-<style>
-    .left{
-        justify-content: center;
-        width: 50%;
-        float: left;
-    }
-    .right{
-        justify-content: center;
-        width: 50%;
-        float: right;
-    }
+<script>
 
+</script>
+
+<style>
+
+.center-box {
+    display: flex; /* Use flexbox for layout */
+    justify-content: center; /* Center the content horizontally */
+    align-items: center; /* Center the content vertically */
+    height: 100vh; /* Take up full viewport height */
+}
+
+
+.content {
+    display: flex; /* Use flexbox for the inner container as well */
+    border: 2px solid black; /* Add a border for visibility */
+    padding: 20px; /* Add some padding around the content */
+}
+
+
+.part {
+    flex: 1; /* Make each part take up equal space */
+    text-align: center; /* Center the text inside each part */
+    padding: 10px; /* Add padding inside each part */
+    border-right: 1px solid black; /* Add a divider between the two parts */
+}
+
+
+.part:last-child {
+    border-right: none;
+}
 </style>
 
-
-<div class = "left">
-    <h1>
-        Hello?
-    </h1>
-
-
-
-
+<div class="center-box">
+<div class="content">
+    <div class="part">Part 1</div>
+    <div class="part">Part 2</div>
 </div>
-<div class = "right">
-    <h1>
-        Hello?
-    </h1>
 </div>
