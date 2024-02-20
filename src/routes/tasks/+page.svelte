4fe6c35f904453c0a2b67dab157a39af74419b84
ZanyeNair <zayne.nair@circuitrunners.com>
2024-02-20 18:51:50 -0500

    cool?

--- src/routes/tasks/+page.svelte
+++ src/routes/tasks/+page.svelte
@@ -2,12 +2,126 @@
 
 <script>
     
-    // import { Button, CalendarView } from "fluent-svelte";
-
-    let value = new Date(); // The current date
+    const date = new Date();
+	
+	const today = {
+		dayNumber: date.getDate(),
+		month: date.getMonth(),
+		year: date.getFullYear(),
+	}
+	
+	const monthNames = [ "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+	let monthIndex = date.getMonth();
+	// const currentMonth = date.toLocaleString('en-US', { month: 'long' })
+	$: month = monthNames[monthIndex];
+	
+	let year = date.getFullYear();
+	
+	$: firstDayIndex = new Date(year, monthIndex, 1).getDay();
+	// const currentDay = date.getDate();
+	$: numberOfDays = new Date(year, monthIndex+1, 0).getDate();
+	
+	$: calendarCellsQty = numberOfDays + firstDayIndex;
+	
+	const goToNextMonth = () => {
+		if (monthIndex >= 11) {
+			year += 1;
+			return monthIndex = 0;
+		}
+		return monthIndex += 1;
+	}
+	
+	const goToPrevMonth = () => {
+		if (monthIndex <= 0) {
+			year -= 1;
+			return monthIndex = 11;
+		}
+		return monthIndex -= 1;
+	}
+	
+	$: console.log(`${month}, ${today.dayNumber}, ${year}, FIRST DAY index is ${firstDayIndex}, MONTH index is ${monthIndex}, No. of days: ${numberOfDays}`)
 </script>
 
 <style>
+
+ul {list-style-type: none;}
+
+	/* Month header */
+	.month {
+		padding: 70px 25px;
+		width: auto;
+		background: #1abc9c;
+		text-align: center;
+	}
+
+	/* Month list */
+	.month ul {
+		margin: 0;
+		padding: 0;
+	}
+
+	.month ul li {
+		color: white;
+		font-size: 20px;
+		text-transform: uppercase;
+		letter-spacing: 3px;
+	}
+
+	/* Previous button inside month header */
+	.month .prev {
+		float: left;
+		padding-top: 10px;
+		cursor: pointer;
+	}
+
+	/* Next button */
+	.month .next {
+		float: right;
+		padding-top: 10px;
+		cursor: pointer;
+	}
+
+	/* Weekdays (Mon-Sun) */
+	.weekdays {
+		margin: 0;
+		padding: 10px 0;
+		background-color:#ddd;
+	}
+
+	.weekdays li {
+		display: inline-block;
+		width: 13.6%;
+		color: #666;
+		text-align: center;
+	}
+
+	/* Days (1-31) */
+	.days {
+		padding: 10px 0;
+		background: #eee;
+		margin: 0;
+	}
+
+	.days li {
+		list-style-type: none;
+		display: inline-block;
+		border: 1px solid black;
+		padding: 9px;
+		width: 11.6%;
+		text-align: center;
+		margin-bottom: 1px;
+		font-size: 1.2rem;
+		color: #777;
+		cursor: pointer;
+	}
+
+	/* Highlight the "current" day */
+	.active {
+		padding: 5px;
+		background: #1abc9c;
+		color: white !important
+	}
+
 
 .center-box {
     display: flex; /* Use flexbox for layout */
@@ -59,6 +173,41 @@
         </form>
 
     </div>
-    <div class="part">Calendar</div>
+    <div class="part">
+        <div class="month">
+		<ul>
+			<li class="prev" on:click={goToPrevMonth}>&#10094;</li>
+			<li class="next" on:click={goToNextMonth}>&#10095;</li>
+			<li>{month}<br>
+				<span style="font-size:18px">{year}</span>
+			</li>
+		</ul>
+	</div>
+
+	<ul class="weekdays">
+		<li>Su</li>
+		<li>Mo</li>
+		<li>Tu</li>
+		<li>We</li>
+		<li>Th</li>
+		<li>Fr</li>
+		<li>Sa</li>
+	</ul>
+
+	<ul class="days">
+		{#each Array(calendarCellsQty) as _, i}
+			{#if i < firstDayIndex || i >= numberOfDays+firstDayIndex  }
+				<li>&nbsp;</li>
+			{:else}
+				<li class:active={i === today.dayNumber+(firstDayIndex-1) &&
+													monthIndex === today.month &&
+													year === today.year}>
+					{(i - firstDayIndex) + 1}
+				</li>
+			{/if}
+		{/each}
+	</ul>    
+        
+    </div>
 </div>
 </div>
