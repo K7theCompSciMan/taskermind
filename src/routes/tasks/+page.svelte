4d62cc31de3271c539e91572297e5a232cbab0e9
ZanyeNair <zayne.nair@circuitrunners.com>
2024-02-21 02:05:28 -0500

    yuh

--- src/routes/tasks/+page.svelte
+++ src/routes/tasks/+page.svelte
@@ -3,28 +3,6 @@
 <style>
 
 
-.center-box {
-    display: flex; /* Use flexbox for layout */
-    justify-content: center; /* Center the content horizontally */
-    height: 100vh; /* Take up full viewport height */
-}
-
-
-.content {
-    display: flex; /* Use flexbox for the inner container as well */
-    border: 2px solid black; /* Add a border for visibility */
-    padding: 20px; /* Add some padding around the content */
-}
-
-
-.part {
-    flex: 1; /* Make each part take up equal space */
-    text-align: center; /* Center the text inside each part */
-    padding: 10px; /* Add padding inside each part */
-    border-right: 1px solid black; /* Add a divider between the two parts */
-}
-
-
 .part:last-child {
     border-right: none;
 }
@@ -36,9 +14,9 @@
   cursor: pointer;
   opacity: 0.8;
   position: fixed;
-  bottom: 23px;
+  /* bottom: 23px;
   right: 28px;
-  width: 280px;
+  width: 280px; */
 }
 
 /* The popup form - hidden by default */
