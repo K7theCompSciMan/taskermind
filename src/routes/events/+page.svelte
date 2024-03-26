ada5ca1c2c65c981c79d3034dec0336128c301ee
ZanyeNair <zayne.nair@circuitrunners.com>
2024-03-26 15:56:19 -0400

    yay

--- src/routes/events/+page.svelte
+++ src/routes/events/+page.svelte
@@ -9,4 +9,24 @@
 
 <Calendar {today} year={years} /> -->
 
+<script>
 
+var types = ["Assigned", "Started", "Submit", "Completed"];
+for (var i = 0; i<types.length; i++){
+	let titles = document.createElement("div");
+	titles.innerHTML = types[i];
+	titles.classList.add("types");
+
+}
+
+
+</script>
+<style>
+
+</style>
+
+
+<body>
+	
+	
+</body>
