35f2d4508392449271b8818220828dac29793a68
K7theCompSciMan <k7rang2018@gmail.com>
2024-10-10 18:00:28 -0400

    testing vue

--- src/App.vue
+++ src/App.vue
@@ -14,6 +14,7 @@ import TheWelcome from './components/TheWelcome.vue'
 
   <main>
     <TheWelcome />
+    <EventCard />
   </main>
 </template>
 
---
+++ src/components/EventCard.vue
@@ -0,0 +1,29 @@
+<!-- Contains code for an EventCard -->
+<template>
+  <div class="event-card">This is an event EventCard</div>
+</template>
+
+<style scoped>
+h1 {
+  font-weight: 500;
+  font-size: 2.6rem;
+  position: relative;
+  top: -10px;
+}
+
+h3 {
+  font-size: 1.2rem;
+}
+
+.event-card h1,
+.event-card h3 {
+  text-align: center;
+}
+
+@media (min-width: 1024px) {
+  .greetings h1,
+  .greetings h3 {
+    text-align: left;
+  }
+}
+</style>
