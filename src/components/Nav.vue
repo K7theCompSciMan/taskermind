7635cad4bcaae23b65761c78481f4fbd820bcf34
K7theCompSciMan <k7rang2018@gmail.com>
2024-11-15 22:18:50 -0500

    updated color scheme and navigation

---
+++ src/components/Nav.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+import { useRouter } from 'vue-router';
+import Button from './NavButton.vue'
+
+const navigate = (path: string) => {
+    useRouter().push(path)
+}
+</script>
+
+<template>
+	<nav>
+		<Button text="Home" @click="navigate('/')"></Button>
+		<Button text="About" @click="navigate('/about')"></Button>
+	</nav>
+</template>
+
+<style>
+nav {
+    display: flex;
+    justify-content: space-around;
+    background-color: var(--color-background);
+    border: 1px solid var(--color-border);
+    width: 100vw;
+    position: sticky;
+    top: 0;
+    padding-top: 1vh;
+}
+
+</style>
---
+++ src/components/NavButton.vue
@@ -0,0 +1,34 @@
+<script setup lang="ts">
+import { defineProps } from 'vue'
+import { RouterLink, useRouter } from 'vue-router'
+
+const props = defineProps({
+    text: {
+        type: String,
+        required: true
+    }
+})
+</script>
+
+<template>
+    <button @click="useRouter().push('/about')">{{text}}</button>
+</template>
+
+
+<style scoped>
+    button {
+        background: transparent;
+        border: 1px solid var(--color-primary);
+        color: var(--color-text);
+        padding: 0.5rem 1rem;
+        border-radius: 0.5rem;
+        cursor: pointer;
+        font-size: 1.25rem;
+        transition: all 0.2s;
+    }
+    button:hover {
+        color: var(--color-accent);
+        scale: 1.1;
+        transition: all 0.2s;
+    }
+</style>
