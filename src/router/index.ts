c04ae4625a126e469e044db15c1caf3788f589f7
ZanyeNair <141592436+ZanyeNair@users.noreply.github.com>
2025-04-07 15:51:55 -0400

    update on tasks page

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import AboutView from '../views/AboutView.vue'
+import TaskView from '../views/TaskView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -18,6 +19,15 @@ const router = createRouter({
       // which is lazy-loaded when the route is visited.
       component: AboutView,
     },
+    {
+      path: '/tasks',
+      name: 'task',
+      // route level code-splitting
+      // this generates a separate chunk (About.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: TaskView,
+    },
+    
   ],
 })
 
---
+++ src/views/TaskView.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="tasks">
+      <h1>This is an task page</h1>
+    </div>
+  </template>
+  
+  <style>
+  @media (min-width: 1024px) {
+    .about {
+      min-height: 100vh;
+      display: flex;
+      align-items: center;
+    }
+  }
+  </style>
+  
