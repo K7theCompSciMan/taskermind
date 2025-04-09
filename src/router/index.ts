4fade565f21c3468e468bc7394eac14d104750a8
ZanyeNair <141592436+ZanyeNair@users.noreply.github.com>
2025-04-08 21:08:54 -0400

    calender view???

--- src/router/index.ts
+++ src/router/index.ts
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import AboutView from '../views/AboutView.vue'
 import TaskView from '../views/TaskView.vue'
+import CalView from '../views/CalView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -26,6 +27,14 @@ const router = createRouter({
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
       component: TaskView,
+    },
+    {
+      path: '/cal',
+      name: 'calender',
+      // route level code-splitting
+      // this generates a separate chunk (About.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: CalView,
     },
     
   ],
---
+++ src/views/CalView.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="p-4">
+      <FullCalendar
+        :plugins="[dayGridPlugin]"
+        initial-view="dayGridMonth"
+        :events="events"
+      />
+    </div>
+  </template>
+  
+  <script setup>
+  import FullCalendar from '@fullcalendar/vue3'
+  import dayGridPlugin from '@fullcalendar/daygrid'
+  
+  // Simple static events
+  const events = [
+    { title: 'Math Test', date: '2025-04-10' },
+    { title: 'Birthday Party', date: '2025-04-15' }
+  ]
+  </script>
+  
