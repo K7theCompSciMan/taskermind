93caf3b66da7ddf245d52eb1a2649fec2f61c585
VidhullaSaravana <90876747+VidhullaSaravana@users.noreply.github.com>
2025-04-09 20:15:09 -0400

    Update HomeView.vue

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,24 @@
-<script setup lang="ts">
+<template>
+  <div>
+    <h1>Welcome to the Home Page</h1>
+    <!-- Button that navigates to the Task View -->
+    <button @click="goToTaskView">Go to Task View</button>
+  </div>
+</template>
 
+
+<script lang="ts">
+export default {
+  name: "Home",
+  methods: {
+    goToTaskView() {
+      // Navigate to TaskView when the button is clicked
+      this.$router.push('/task');
+    }
+  }
+};
 </script>
 
-<template>
-  <main>
-    home
-  </main>
-</template>
+<style scoped>
+/* Add home page specific styles here */
+</style>
